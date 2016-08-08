37da4473bacbd2487c3c4b1d907e26bbaa4a93a2
Scott Yiu <yysy2@cam.ac.uk>
2016-08-08 16:09:21 +0100

    Completed exercise

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #print(x)
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+k <- matrix((c(3,2,0,0,0,1,2,-2,1)), nrow =3, ncol=3)
+#print(k)
+myMatrix <- makeCacheMatrix(k)
+myMatrix[[2]]()
+cacheSolve(myMatrix)
+
+#myVector <- makeVector(1:15)
+#myVector[[2]]()
+#cachemean(myVector)
---
+++ test.R
@@ -0,0 +1,67 @@
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+myVector <- makeVector(1:15)
+myVector[[2]]()
+cachemean(myVector)
+
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function() m <<- mean(x)
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+myVector <- makeVector(1:15)
+myVector[[2]]()
+cachemean(myVector)
+
+
+#power <- function(exponent) {
+#  function(x) {
+#    x ^ exponent
+#  }
+#}
